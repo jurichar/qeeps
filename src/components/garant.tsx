29aee17ab5fe9a83c3eb92b1472d00ab6db1779f
Julien Richard <45456851+jurichar@users.noreply.github.com>
2023-12-04 11:26:52 +0100

    Refactor HTML structure and update styles

--- src/components/garant.tsx
+++ src/components/garant.tsx
@@ -3,29 +3,29 @@ import JustificativesComponent from "./justif"
 function GarantComponent() {
     return (
         <div>
-            <h1 style={{ fontSize: '1.25rem' }} >Garant : Alban Colin Bricard</h1>
-            <div style={{ fontSize: '0.875rem', fontWeight: '300' }} className='rounded-lg border border-grayGray-10 bg-subtleWhite p-4'>
+            <h1 style={{ fontSize: '1.25rem' }} className=''>Garant : Alban Colin Bricard</h1>
+            <div style={{ fontSize: '0.875rem', fontWeight: '300' }} className='rounded-lg border border-grayGray-10 bg-subtleWhite p-4 my-5'>
                 <div className="flex justify-between">
                     <p>Nom complet</p>
                     <p>Alban Colin Bricard</p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between  my-4">
                     <p>Téléphone</p>
                     <p>07 44 35 45 22</p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between  my-4">
                     <p>Relation</p>
                     <p>Père</p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between  my-4">
                     <p>Mail</p>
                     <p>alban.c.bricard3@gmail.com</p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between  my-4">
                     <p>Situation professionnelle</p>
                     <p>Retraité propriétaire</p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between  my-4">
                     <p>
                         Revenus mensuel net
                         <br />
--- src/components/myFile.tsx
+++ src/components/myFile.tsx
@@ -3,16 +3,16 @@ import JustificativesComponent from "./justif"
 function MyFileComponent() {
     return (
         <div>
-            <div style={{ fontSize: '0.875rem', fontWeight: '300' }} className='rounded-lg border border-grayGray-10 bg-subtleWhite p-4'>
-                <div className="flex justify-between">
+            <div style={{ fontSize: '0.875rem', fontWeight: '300' }} className='rounded-lg border border-grayGray-10 bg-subtleWhite p-4 my-5'>
+                <div className="flex justify-between my-4">
                     <p>Téléphone</p>
                     <p>06 88 56 45 23</p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between my-4">
                     <p>Mail</p>
                     <p>fabien.bricard932@gmail.com</p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between my-4">
                     <p>Situation professionnelle</p>
                     <p>
                         CDI (Periode d'essai terminée)
@@ -20,7 +20,7 @@ function MyFileComponent() {
                         Prise de fonction le 17/03/2018
                     </p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between my-4">
                     <p>
                         Revenus mensuel net
                         <br />
@@ -28,7 +28,7 @@ function MyFileComponent() {
                     </p>
                     <p>2100€</p>
                 </div>
-                <div className="flex justify-between">
+                <div className="flex justify-between my-4">
                     <p>Autres revenus</p>
                     <p>150€ (Rente)</p>
                 </div>
